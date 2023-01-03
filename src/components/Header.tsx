fb8a4ca14e64d89c54e53877533b34e3b89f3e06
David Aronmwan <davearonmwan@gmail.com>
2023-01-03 22:27:17 +0100

    fix: changed logo path

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header() {
   return (
     <header>
-      <img src="../public/assets/logo.svg" alt="Logo" className="logo" />
+      <img src="./assets/logo.svg" alt="Logo" className="logo" />
       <p>Dave Project 3</p>
     </header>
   );
